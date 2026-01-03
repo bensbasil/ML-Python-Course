006fb781fbad04e1d278abbd14e83e44173413f9
Bens Basil <bensdbasil@gmail.com>
2026-01-03 07:59:12 +0530

    completed memory management and started leaning Flask Framework

---
+++ ML-Flask/flask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+'''
+create an instance of the flask class,
+which will be your WSGI(Webserver Gateway Interface) application
+'''
+### WSGI Application
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to this Flask course. This should be an amazing course.p"
+
+@app.route("/index")
+def index():
+    return "Lets learn ML"
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+

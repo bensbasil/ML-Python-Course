28e41ad38281f541d4d435d140f1f71a5bade740
Bens Basil <bensdbasil@gmail.com>
2026-01-05 16:30:23 +0530

    Streamlit Webframework Completed

---
+++ ML-StreamLit/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#Title of application
+st.title("Hello Streamlit")
+
+#Display Simple Text
+st.write("This is a simple text")
+
+##create a simple Dataframe
+
+df=pd.DataFrame({
+    "First column":[1,2,3,4,5],
+    "Second column":[10,20,30,40,50]
+})
+
+##Display the DataFrame
+st.write("Here is the dataframe")
+st.write(df)
+
+#Create a line chart
+chart_data=pd.DataFrame(
+    np.random.randn(20,3),columns=['a','b','c']
+)
+st.line_chart(chart_data)
